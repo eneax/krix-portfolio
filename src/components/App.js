a1517c24ee794d995865ca112250cfce9c366469
eneax <19818937+eneax@users.noreply.github.com>
2018-06-24 22:17:57 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import 'tachyons';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        home
+      </div>
+    );
+  }
+}
+
+export default App;
