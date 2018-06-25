1b7e7c1914860475e1638e5c1cb6add44c06a2e4
eneax <19818937+eneax@users.noreply.github.com>
2018-06-25 16:38:50 +0200

    Style Logo and Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 	return (
-    <nav class="dt w-100 border-box pa3 ph5-ns">
-      <Link to="/" className="dtc v-mid mid-gray link dim w-25">
-        <img src="http://tachyons.io/img/logo.jpg" class="dib w2 h2 br-100" alt="Site Name" />
+    <nav className='dt w-100 border-box pa3 ph5-ns pv5'>
+      <Link to='/' className='dtc v-mid mid-gray link dim w-25'>
+        <img src='http://tachyons.io/img/logo.jpg' className='dib w3 h3 br-100' alt='Kri Xharja Logo' />
       </Link>
-      <div class="dtc v-mid w-75 tr">
-        <Link to="/about" className="link dim dark-gray f6 f5-ns dib">
+      <div className='dtc v-mid w-75 tr'>
+        <Link to='/about' className='link dim dark-gray f6 f5-ns dib'>
           About
         </Link>
       </div>
