5d80e1067b7a0794d95080f17fe12c222749cf85
eneax <19818937+eneax@users.noreply.github.com>
2018-06-25 21:08:42 +0200

    Import images and create media file

---
+++ src/utils/media.js
@@ -0,0 +1,14 @@
+/* Logo */
+// import logo from '../images/logo.png';
+
+/* Section Home */
+import projectOne from '../images/project1.jpg';
+import projectTwo from '../images/project2.jpg';
+import projectThree from '../images/project3.jpg';
+import projectFour from '../images/project4.jpg';
+
+
+export {
+  // logo,
+  projectOne, projectTwo, projectThree, projectFour
+}
