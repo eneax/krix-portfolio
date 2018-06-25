5ee74d0533b8663f0b695c1fad05c23544c28cf4
eneax <19818937+eneax@users.noreply.github.com>
2018-06-25 16:43:01 +0200

    Create About component

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import {
 import 'tachyons';
 import Navbar from './Navbar';
 import Home from './Home';
+import About from './About';
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
           <Navbar />
           <Switch>
             <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
           </Switch>
         </div>
       </Router>
