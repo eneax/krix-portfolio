bea5ee6180797884eaa39e3f47e3cb5fd2366fe5
eneax <19818937+eneax@users.noreply.github.com>
2018-06-29 21:20:53 +0200

    Add Links to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,62 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { projectOne, projectTwo, projectThree, projectFour } from '../utils/media';
 import Footer from './Footer';
 
 const Home = () => {
 	return (
 		<div className='mw9 center ph3-ns'>
+      <div className='cf ph2-ns'>
+        
+        <Link to='/projectOne'>
+          <div className='fl w-100 w-50-ns pa2'>
+            <div className='outline bg-white pv4'>
+              <img src={projectOne} alt="Project One Pic" />
+            </div>
+          </div>
+        </Link>
+        
+        <Link to='/projectTwo'>
+          <div className='fl w-100 w-50-ns pa2'>
+            <div className='outline bg-white pv4'>
+              <img src={projectTwo} alt="Project Two Pic" />
+            </div>
+          </div>
+        </Link>
+
+      </div>
+      
+      <div className='cf ph2-ns'>
+        
+        <Link to='/projectThree'>
+          <div className='fl w-100 w-50-ns pa2'>
+            <div className='outline bg-white pv4'>
+              <img src={projectThree} alt="Project Three Pic" />
+            </div>
+          </div>
+        </Link>
+
+        <Link to='/projectFour'>
+          <div className='fl w-100 w-50-ns pa2'>
+            <div className='outline bg-white pv4'>
+              <img src={projectFour} alt="Project Four Pic" />
+            </div>
+          </div>
+        </Link>
+
+      </div>
+
+      <Footer />
+    </div>
+	)
+}
+
+export default Home;
+
+
+/*
+
+<div className='mw9 center ph3-ns'>
       <div className='cf ph2-ns'>
         <div className='fl w-100 w-50-ns pa2'>
           <div className='outline bg-white pv4'>
@@ -32,7 +84,5 @@ const Home = () => {
 
       <Footer />
     </div>
-	)
-}
 
-export default Home;
+*/ 
