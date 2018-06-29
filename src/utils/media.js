810577167a9ed94372e180acea164e100d792e50
eneax <19818937+eneax@users.noreply.github.com>
2018-06-29 21:11:39 +0200

    Restructure img folder

--- src/utils/media.js
+++ src/utils/media.js
@@ -2,13 +2,13 @@
 // import logo from '../images/logo.png';
 
 /* Section Home */
-import projectOne from '../images/project1.jpg';
-import projectTwo from '../images/project2.jpg';
-import projectThree from '../images/project3.jpg';
-import projectFour from '../images/project4.jpg';
+import projectOne from '../images/cover1.jpg';
+import projectTwo from '../images/cover2.jpg';
+import projectThree from '../images/cover3.jpg';
+import projectFour from '../images/cover4.jpg';
 
 /* Section About */
-import profilePic from '../images/profile-pic.JPG';
+import profilePic from '../images/profile-pic.png';
 
 export {
   // logo,
