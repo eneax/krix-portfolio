cab5f51b07ea9c7be7adb5717a94e2dcde286e07
eneax <19818937+eneax@users.noreply.github.com>
2018-07-01 17:16:26 +0200

    Add logo to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { logo } from '../utils/media';
 
 const Navbar = () => {
 	return (
-    <nav className='dt w-100 border-box pa3 ph4-ns pv5'>
+    <nav className='dt w-100 border-box pa3 ph4-ns pv4'>
       <Link to='/' className='dtc v-mid mid-gray link dim w-25'>
-        <img src='http://tachyons.io/img/logo.jpg' className='dib w3 h3 br-100' alt='Kri Xharja Logo' />
+        <img src={logo} className='logo dib br-100' alt='Kri Xharja Logo' />
       </Link>
       <div className='dtc v-mid w-75 tr'>
+        <Link to='/projects' className='link dim dark-gray f6 f5-ns dib mr3 mr4-ns'>
+          Projects
+        </Link>
         <Link to='/about' className='link dim dark-gray f6 f5-ns dib'>
           About
         </Link>
@@ -18,3 +22,4 @@ const Navbar = () => {
 
 export default Navbar;
 
+// <img src='http://tachyons.io/img/logo.jpg' className='dib w3 h3 br-100' alt='Kri Xharja Logo' />
