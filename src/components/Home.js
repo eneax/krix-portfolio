18287e2bc571c5716c903d857bcba8eee60b7938
eneax <19818937+eneax@users.noreply.github.com>
2018-07-02 22:13:27 +0200

    Add dynamic text-shadow to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,45 @@
 import React from 'react';
 
-const home = () => {
-  return (
-    <main className='pt4'>
-      <div className="tc pv4 pv6-ns">
-        <h1 className="f4 f3-ns fw6 dark-gray courier">Hi, I'm Kristjana!</h1>
-        <h2 className="f6 mid-gray fw2 ttu tracked georgia">Architect Visualizer</h2>
+class Home extends React.Component {
+  componentDidMount() {
+    const hero = document.querySelector('.hero');
+    const text = hero.querySelector('div');
+    const walk = 4; // 4px
 
-        <a className="f6 grow dib v-mid ph3 pv2 mb3 dark-gray" href="https://www.facebook.com/kristjana.xharja" target="_blank" rel="noopener noreferrer" >
-          <i className="fab fa-facebook-f fa-2x"></i>
-        </a>
-        <a className="f6 grow dib v-mid ph3 pv2 mb3 dark-gray" href="mailto:kristjana.xharja@gmail.com" target="_blank" rel="noopener noreferrer" >
-          <i className="fas fa-envelope fa-2x"></i>
-        </a>
-      </div>
-    </main>
-	)
+    function shadow(e) {
+      const { offsetWidth: width, offsetHeight: height } = hero;
+      let { offsetX: x, offsetY: y } = e;
+
+      if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+      }
+
+      const xWalk = Math.round((x / width * walk) - (walk / 2));
+      const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+      text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(0, 0, 0, .15)`;
+    }
+
+    hero.addEventListener('mousemove', shadow);
+  }
+  render() {
+    return (
+      <main className='pt4 hero'>
+        <div className="tc pv4 pv6-ns">
+          <h1 className="f4 f3-ns fw6 dark-gray avenir">Hi, I'm Kristjana!</h1>
+          <h2 className="f6 mid-gray fw2 ttu tracked baskerville">Architect Visualizer</h2>
+  
+          <a className="f6 grow dib v-mid ph3 pv2 mb3 dark-gray" href="https://www.facebook.com/kristjana.xharja" target="_blank" rel="noopener noreferrer" >
+            <i className="fab fa-facebook-f fa-2x"></i>
+          </a>
+          <a className="f6 grow dib v-mid ph3 pv2 mb3 dark-gray" href="mailto:kristjana.xharja@gmail.com" target="_blank" rel="noopener noreferrer" >
+            <i className="fas fa-envelope fa-2x"></i>
+          </a>
+        </div>
+      </main>
+    )
+  }
 }
 
-export default home;
+export default Home;
